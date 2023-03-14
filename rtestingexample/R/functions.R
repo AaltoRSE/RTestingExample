20ff3869423cb730f2e37f0178de9d14cb63043c
Thomas Pfau <github@thomaspfau.de>
2023-03-14 04:31:43 +0200

    Updated tests so that they pass

--- rtestingexample/R/functions.R
+++ rtestingexample/R/functions.R
@@ -4,14 +4,14 @@ add <- function(a,b){
 }
 
 subtract <- function(a,b){
-    return(a + b);
+    return(a - b);
 }
 
 multiply <- function(a,b){
-    return(a + b);
+    return(a * b);
 }
 
 convert_fahrenheit_to_celsius <- function(fahrenheit){
-    C_temp <- multiply(subtract(fahrenheit,32),9/5);
+    C_temp <- multiply(subtract(fahrenheit,32),5/9);
     return(C_temp);
 }
